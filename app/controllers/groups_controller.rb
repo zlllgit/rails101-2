f2f86762250e92b1e2366ea3dad7e4982e10dcb5
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-03-13 14:04:49 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,6 +23,7 @@ end
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
